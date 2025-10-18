d786e70f6e8ec9700f2ab252996a5b4b74ad50d2
Beatrice Olaosebikan <beat1rice2ola@gmail.com>
2025-10-18 14:55:54 -0500

    1st commit

---
+++ ToyotaHackTx/backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import recommendRouter from "./routes/recommend.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/recommend", recommendRouter);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
